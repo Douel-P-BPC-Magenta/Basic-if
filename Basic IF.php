d04de7a02238aa78f66446c02328a66a365a4fda
Patrick Douel <32098830+Douel-P-BPC-Magenta@users.noreply.github.com>
2017-10-31 13:36:18 +0000

    Add files via upload

---
+++ Basic IF.php
@@ -0,0 +1,27 @@
+	<html>
+	<head>
+	<title>test 01</title>
+	</head>
+	<body>
+	<?php
+	$fName = "deb";
+	$sName = "McWhin";
+	$fullname = $fName . " " . $sName;
+	
+	echo $fullname;
+	
+	if ($fullname == "bigloser") {
+	echo "holy moly, thats a great name";
+	}
+	elseif
+	($fullname == "deb McWhin") {
+	echo "DABDABDAB";
+	}
+	else
+	{
+	echo "You're one crazy boi";
+	}
+	
+	?>
+	</body>
+	</html>
